859aca41278e8e42fcb5307489c1df6a0faa7c45
Daniel Krupiński <danielkrupinski@outlook.com>
2019-08-29 17:16:52 +0200

    Add icePbox array

--- VAC/Utils.c
+++ VAC/Utils.c
@@ -58,7 +58,7 @@ PBYTE Utils_memset(PBYTE dest, INT value, INT size)
 // 8B 44 24 0C 53
 INT Utils_strncmp(PBYTE str1, PBYTE str2, SIZE_T count)
 {
-    for (INT i = 0; i < count; i++)
+    for (SIZE_T i = 0; i < count; i++)
         if (str1[i] != str2[i])
             return str1[i] - str2[i];
     return 0;
@@ -104,3 +104,15 @@ INT Utils_compareStringW(PCNZWCH string1 , PCNZWCH string2, INT count)
 {
     return CompareStringW(LOCALE_SYSTEM_DEFAULT, NORM_IGNORECASE, string1, count, string2, count) - CSTR_EQUAL;
 }
+
+static const UINT icePbox[32] = {
+    0x00000001, 0x00000080, 0x00000400, 0x00002000,
+    0x00080000, 0x00200000, 0x01000000, 0x40000000,
+    0x00000008, 0x00000020, 0x00000100, 0x00004000,
+    0x00010000, 0x00800000, 0x04000000, 0x20000000,
+    0x00000004, 0x00000010, 0x00000200, 0x00008000,
+    0x00020000, 0x00400000, 0x08000000, 0x10000000,
+    0x00000002, 0x00000040, 0x00000800, 0x00001000,
+    0x00040000, 0x00100000, 0x02000000, 0x80000000 };
+
+
