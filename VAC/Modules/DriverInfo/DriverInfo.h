9c61d9b29813c1468cea10d30203e9855b64aba1
Daniel Krupiński <danielkrupinski@outlook.com>
2019-11-26 21:54:34 +0100

    Add DriverInfo struct

--- VAC/Modules/DriverInfo/DriverInfo.h
+++ VAC/Modules/DriverInfo/DriverInfo.h
@@ -8,5 +8,26 @@ BOOLEAN DriveInfo_getFileInfo(PCWSTR, DWORD*, DWORD[2]);
 // E8 ? ? ? ? 89 44 24 10 (relative jump)
 LPCWSTR DriverInfo_findSystem32InString(PCWSTR);
 
+typedef struct DriverInfo {
+    DWORD unknown[4];
+    DWORD mystery; // initialized to 0x6E1CA4EA
+    DWORD scanResult; // return value from DriverInfo_getDriverInfo() or error code
+    DWORD zero; // initialized to 0
+
+    /* Members accessed by DriverInfo_getDriverInfo() */
+    CHAR serviceName[256];
+    CHAR displayName[256];
+    DWORD serviceType;
+    DWORD startType;
+    DWORD errorControl;
+    CHAR driverPath[256];
+    CHAR loaderOrderGroup[32];
+    CHAR dependencies[256];
+    CHAR serviceStartName[32];
+    DWORD fileIndex[2];
+    DWORD volumeSerial;
+    /* ---------------------------------------------- */
+} DriverInfo;
+
 // 81 EC ? ? ? ? 53
 DWORD DriverInfo_getDriverInfo(DWORD* data, INT driverNameHash);
