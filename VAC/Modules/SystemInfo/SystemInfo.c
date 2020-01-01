c6494b0ee3a6e1a9be6594d02b824f0713e1a1be
Daniel Krupiński <danielkrupinski@outlook.com>
2020-01-01 17:52:30 +0100

    Update SystemInfo_collectData() function

--- VAC/Modules/SystemInfo/SystemInfo.c
+++ VAC/Modules/SystemInfo/SystemInfo.c
@@ -5,6 +5,7 @@ INT SystemInfo_collectData(PVOID unk, PVOID unk1, PVOID unk2, PVOID unk3)
 {
     CHAR ntDll[] = "\x68\x52\x62\x4A\x4A\x8\x42\x4A\x4A";
     CHAR kernel32[] = "\x6D\x43\x54\x48\x43\x4A\x15\x14\x8\x42\x4A\x4A";
+    CHAR ntQuerySystemInformation[] = "\x68\x52\x77\x53\x43\x54\x5F\x75\x5F\x55\x52\x43\x4B\x6F\x48\x40\x49\x54\x4B\x47\x52\x4F\x49\x48";
 
     PCHAR curr = ntDll;
     while (*curr) {
@@ -18,6 +19,11 @@ INT SystemInfo_collectData(PVOID unk, PVOID unk1, PVOID unk2, PVOID unk3)
         ++curr;
     }
 
+    curr = ntQuerySystemInformation;
+    while (*curr) {
+        *curr ^= '&';
+        ++curr;
+    }
 
     return 0;
 }
