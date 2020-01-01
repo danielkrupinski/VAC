b01957f8dd0b950d21db23f618eb3e7f64aeee34
Daniel Krupiński <danielkrupinski@outlook.com>
2020-01-01 17:28:31 +0100

    Add SystemInfo_collectData() function

---
+++ VAC/Modules/SystemInfo/SystemInfo.c
@@ -0,0 +1,7 @@
+#include "SystemInfo.h"
+
+// 55 8B EC B8
+INT SystemInfo_collectData(PVOID unk, PVOID unk1, PVOID unk2, PVOID unk3)
+{
+    return 0;
+}
--- VAC/Modules/SystemInfo/SystemInfo.h
+++ VAC/Modules/SystemInfo/SystemInfo.h
@@ -1,1 +1,6 @@
 #pragma once
+
+#include <Windows.h>
+
+// 55 8B EC B8
+INT SystemInfo_collectData(PVOID unk, PVOID unk1, PVOID unk2, PVOID unk3);
