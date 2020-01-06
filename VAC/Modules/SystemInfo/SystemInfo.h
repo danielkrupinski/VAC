2cb6f7c0eb7f8d7d9b524a74109d838ee266212c
Daniel Krupiński <danielkrupinski@outlook.com>
2020-01-06 19:22:17 +0100

    Add VolumeData struct

--- VAC/Modules/SystemInfo/SystemInfo.h
+++ VAC/Modules/SystemInfo/SystemInfo.h
@@ -2,11 +2,23 @@
 
 #include <Windows.h>
 
+typedef struct VolumeData {
+    UINT volumeGuidHash;
+    DWORD getVolumeInformationError;
+    DWORD fileSystemFlags;
+    DWORD volumeSerialNumber;
+    UINT volumeNameHash;
+    UINT fileSystemNameHash;
+    WORD driveType;
+    WORD volumePathNameLength;
+    DWORD volumePathNameHash;
+} VolumeData;
+
 typedef struct SystemInfo {
     DWORD unknown[4];
     DWORD scanType; // initialized to 0xA93E4B10
     DWORD scanError;
-
+    // ...
 } SystemInfo;
 
 // 55 8B EC B8
