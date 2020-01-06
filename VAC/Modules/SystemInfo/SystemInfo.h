22a244f70d061e88bf1f08bcdf4fa70a7c1d5a73
Daniel Krupiński <danielkrupinski@outlook.com>
2020-01-06 19:01:20 +0100

    Add SystemInfo struct

--- VAC/Modules/SystemInfo/SystemInfo.h
+++ VAC/Modules/SystemInfo/SystemInfo.h
@@ -2,6 +2,13 @@
 
 #include <Windows.h>
 
+typedef struct SystemInfo {
+    DWORD unknown[4];
+    DWORD scanType; // initialized to 0xA93E4B10
+    DWORD scanError;
+
+} SystemInfo;
+
 // 55 8B EC B8
 INT SystemInfo_collectData(PVOID unk, PVOID unk1, DWORD data[2048], PDWORD dataSize);
 
