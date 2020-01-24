cee674dc5d9e2620dab6964360ea658e5f8f1fb3
Daniel Krupiński <danielkrupinski@outlook.com>
2020-01-24 21:11:07 +0100

    Add ProcessMonitor_readFileMapping() function

---
+++ VAC/Modules/ProcessMonitor/ProcessMonitor.c
@@ -0,0 +1,36 @@
+#include "ProcessMonitor.h"
+
+PVOID ProcessMonitor_readFileMapping(PBOOLEAN md5Computed, PBYTE md5, DWORD out[2])
+{
+    PVOID result = NULL;
+    CHAR name[60];
+
+    wsprintfA(name, "Steam_{E9FD3C51-9B58-4DA0-962C-734882B19273}_Pid:%000008X", GetCurrentProcessId());
+
+    if (md5Computed)
+        *md5Computed = FALSE;
+
+    HANDLE fileMapping = OpenFileMappingA(FILE_MAP_READ, FALSE, name);
+
+    if (fileMapping) {
+        DWORD* mapped = MapViewOfFile(fileMapping, FILE_MAP_READ, 0, 0, 0);
+
+        if (mapped) {
+            if (out) {
+                out[0] = mapped[0];
+                out[1] = mapped[1];
+            }
+            if (mapped[0] == 0x30004) // magic number set by steamservice.dll when creating file mapping
+                result = (PVOID)mapped[1];
+            if (md5) {
+                if (md5Computed)
+                    *md5Computed = TRUE;
+
+                // compute md5
+            }
+            UnmapViewOfFile(mapped);
+        }
+        CloseHandle(fileMapping);
+    }
+    return result;
+}
--- VAC/Modules/ProcessMonitor/ProcessMonitor.h
+++ VAC/Modules/ProcessMonitor/ProcessMonitor.h
@@ -1,1 +1,5 @@
 #pragma once
+
+#include <Windows.h>
+
+PVOID ProcessMonitor_readFileMapping(PBOOLEAN md5Computed, PBYTE md5, DWORD out[2]);
