26491394e13f3140e8499006bff03a0bfc3f77fd
Daniel Krupiński <danielkrupinski@outlook.com>
2020-01-26 21:27:21 +0100

    Update ProcessMonitor

--- VAC/Modules/ProcessMonitor/ProcessMonitor.c
+++ VAC/Modules/ProcessMonitor/ProcessMonitor.c
@@ -1,5 +1,6 @@
 #include "ProcessMonitor.h"
 
+// E8 ? ? ? ? 59 8B F8 (relative jump)
 PVOID ProcessMonitor_readFileMapping(PBOOLEAN md5Computed, PBYTE md5, DWORD out[2])
 {
     PVOID result = NULL;
--- VAC/Modules/ProcessMonitor/ProcessMonitor.h
+++ VAC/Modules/ProcessMonitor/ProcessMonitor.h
@@ -2,4 +2,5 @@
 
 #include <Windows.h>
 
+// E8 ? ? ? ? 59 8B F8 (relative jump)
 PVOID ProcessMonitor_readFileMapping(PBOOLEAN md5Computed, PBYTE md5, DWORD out[2]);
